f2f02e3e4a88338bb8a0b905bb648f4d2a5f9575
saurabh.s.ed <saurabh.singh@edvanta.com>
2024-05-12 00:11:21 +0530

    first commit

---
+++ src/Util/ServerPath.js
@@ -0,0 +1,10 @@
+export const baseURL = "http://localhost:3300/";
+export const devKey = "jaZfYL80ZkXc2mmUfVv6BzcuZsNXK5dm";
+
+//aws credentials
+export const ProjectName = "YOUR_PROJECT_NAME";
+export const Bucket_name = "YOUR_BUCKET_NAME";
+export const space_End_point = "YOUR_END_POINT";
+export const aws_access_key_id = "AWS_ACCESS_KEY_ID";
+export const aws_secret_access_key = "YOUR_SECRET_ACCESS_KEY";
+export const folderStructurePath = "YOUR_FOLDER_STRUCTURE_PATH";
