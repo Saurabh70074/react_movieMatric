2c3fcb89c3f8f329b1b973b68804298ae148e444
saurabh.s.ed <saurabh.singh@edvanta.com>
2024-05-14 02:43:11 +0530

    add aws url

--- src/Util/ServerPath.js
+++ src/Util/ServerPath.js
@@ -1,10 +1,12 @@
-export const baseURL = "https://api.moviematric.com/";
+export const baseURL = "https://api.moviematric.com";
 export const devKey = "jaZfYL80ZkXc2mmUfVv6BzcuZsNXK5dm";
 
 //aws credentials
 export const ProjectName = "MOVIE MATRIC";
-export const Bucket_name = "YOUR_BUCKET_NAME";
-export const space_End_point = "YOUR_END_POINT";
-export const aws_access_key_id = "AWS_ACCESS_KEY_ID";
-export const aws_secret_access_key = "YOUR_SECRET_ACCESS_KEY";
-export const folderStructurePath = "YOUR_FOLDER_STRUCTURE_PATH";
+export const Bucket_name = "moviematric";
+export const space_End_point = "";
+export const aws_access_key_id = "AKIAU6GXBXQEOYTU7JJB";
+export const aws_secret_access_key = "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";
+export const folderStructurePath = "";
+
+
